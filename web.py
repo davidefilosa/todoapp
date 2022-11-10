1a58e12cc3d2af024a0c849ccdfb880c8517f71f
Davide Filosa <davideclaypool@gmail.com!>
2022-11-10 11:59:33 +0100

    Implemented add function to web app

--- web.py
+++ web.py
@@ -3,9 +3,22 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
+todos = functions.get_todos()
+
 st.title('My To-do App')
 st.subheader('This are my to-do for today:')
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Enter a todo...')
+st.text_input(label='Enter a todo',
+              placeholder='Enter a todo...',
+              label_visibility='hidden',
+              on_change=add_todo,
+              key='new_todo')
